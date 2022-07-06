8f5995371f0f0c73a0830abe3334a6b529d3a25e
Kleizjd <jose.jdgo98@gmail.com>
2022-07-06 00:26:52 -0500

    descomente

--- Assets/js/functions_usuarios.js
+++ Assets/js/functions_usuarios.js
@@ -364,21 +364,21 @@ function fntDelUsuario(idpersona){
 }
 
 
-// function openModal()
-// {
-//     rowTable = "";
-//     document.querySelector('#idUsuario').value ="";
-//     document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
-//     document.querySelector('#btnActionForm').classList.replace("btn-info", "btn-primary");
-//     document.querySelector('#btnText').innerHTML ="Guardar";
-//     document.querySelector('#titleModal').innerHTML = "Nuevo Usuario";
-//     document.querySelector("#formUsuario").reset();
-//     $('#modalFormUsuario').modal('show');
-// }
+function openModal()
+{
+    rowTable = "";
+    document.querySelector('#idUsuario').value ="";
+    document.querySelector('.modal-header').classList.replace("headerUpdate", "headerRegister");
+    document.querySelector('#btnActionForm').classList.replace("btn-info", "btn-primary");
+    document.querySelector('#btnText').innerHTML ="Guardar";
+    document.querySelector('#titleModal').innerHTML = "Nuevo Usuario";
+    document.querySelector("#formUsuario").reset();
+    $('#modalFormUsuario').modal('show');
+}
 
-// function openModalPerfil(){
-//     $('#modalFormPerfil').modal('show');
-// }
+function openModalPerfil(){
+    $('#modalFormPerfil').modal('show');
+}
 // $(document).ready(function () {  
 //   $("#modalFormUsuario").on("hidden.bs.modal", function (e) {
 //     jQuery(this).removeData("bs.modal");
